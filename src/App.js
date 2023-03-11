3a8a250abd615cb937352a168df762f175d2519d
ali <alifront2233@gmail.com>
2023-03-11 20:24:29 +0330

    create routes and pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import logo from './logo.svg';
+import {BrowserRouter,Route,Router,Routes} from 'react-router-dom';
 import './App.css';
+
+
+
+// ================   pages ===================\\
+
+
+import Home from './pages/Home';
+import ForgotPassword from './pages/ForgotPassword';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import Offers from './pages/Offers';
+
+//---------------------------------------------\\
 
 function App() {
   return (
       <>
-      <h1 className='text-2xl bg-red-500'>Hello world</h1>
+
+
+    <BrowserRouter>
+       <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+          <Route path='/sign-in' element={<SignIn/>}/>
+          <Route path='/sign-up' element={<SignUp/>}/>
+          <Route path='/forgot-password' element={<ForgotPassword/>}/>
+          <Route path='/offers' element={<Offers/>}/>
+        </Routes>
+    </BrowserRouter>
+     
+      
       </>
   );
 }
